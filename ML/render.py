b37d3c3f167e65c91a943eea5095fb7790ca3924
kyanmahajan <kyanmahajan37@gmail.com>
2026-03-02 01:41:50 +0530

    added weight model and backend api along with demo frontend

--- ML/render.py
+++ ML/render.py
@@ -6,7 +6,7 @@ CORS(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index2.html")
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=7861)
